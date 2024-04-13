7696cf4cb6138c4bdec3034aaedd3f4bfe257839
Ahmet Tuna POTUR <tpotur@gmail.com>
2024-04-13 22:17:53 +0300

    fetch and build success

--- app/page.js
+++ app/page.js
@@ -1,12 +1,27 @@
-import Image from "next/image";
+import getAllSuccess from "@/lib/getAllSuccess";
+import getUserByEmail from "@/lib/getUserByEmail";
 
-export default function Home() {
+export default async function Home() {
+  const user = await getUserByEmail("tunapotur@yahoo.com");
+  const allSuccessList = await getAllSuccess();
+
   return (
     <>
-      <div className="mb-6 flex flex-row items-center justify-between px-4">
+      <div className="mb-8 flex flex-row items-center justify-between px-4">
         <div
           className={"flex w-full flex-row items-center justify-between px-2"}
-        ></div>
+        >
+          <div className="flex flex-col">
+            <h1 className="mb-6">{user.name}</h1>
+
+            {allSuccessList.map((success) => (
+              <div key={success._id} className="mb-4 flex flex-col">
+                <span>{success._id}</span>
+                <span>{success.header}</span>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
       <div></div>
     </>
